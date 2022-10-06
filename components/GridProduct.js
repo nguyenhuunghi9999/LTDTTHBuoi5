9084772c5d5346ddf7f51b11ea4030688dae351d
nguyenhuunghi9999 <80930428+nguyenhuunghi9999@users.noreply.github.com>
2022-10-06 16:45:17 +0700

    updateComponent

--- components/GridProduct.js
+++ components/GridProduct.js
@@ -1,70 +1,97 @@
 import { View, Text, FlatList, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import { AirbnbRating } from "react-native-ratings";
 const data = [
   {
-    name: "Ca nấu lẩu, nấu mì mi mini",
-    producer: "Devang",
-    imgLink: require("../assets/ca_nau_lau.png"),
+    id: 7,
+    name: "Cáp chuyển từ Cổng USB sang PS2",
+    rating: 4,
+    price: 69900,
+    imgLink: require("../assets/carbusbtops21.png"),
   },
   {
-    name: "1KG KHÔ GÀ BƠ TỎI",
-    producer: "Devang",
-    imgLink: require("../assets/ga_bo_toi.png"),
+    id: 6,
+    name: "Cáp chuyển từ Cổng USB sang PS2",
+    rating: 4,
+    price: 69900,
+    imgLink: require("../assets/daucam1.png"),
   },
   {
-    name: "Xe cần cẩu đa năng",
-    producer: "Devang",
-    imgLink: require("../assets/xa_can_cau.png"),
+    id: 5,
+    name: "Cáp chuyển từ Cổng USB sang PS2",
+    rating: 4,
+    price: 69900,
+    imgLink: require("../assets/dauchuyendoi1.png"),
   },
   {
-    name: "Đồ chơi dạng mô hình",
-    producer: "Thế giới đồ chơi",
-    imgLink: require("../assets/do_choi_dang_mo_hinh.png"),
+    id: 4,
+    name: "Cáp chuyển từ Cổng USB sang PS2",
+    rating: 4,
+    price: 69900,
+    imgLink: require("../assets/giacchuyen1.png"),
   },
   {
-    name: "Lãnh đạo giản đơn",
-    producer: "Minh Long Book",
-    imgLink: require("../assets/lanh_dao_gian_don.png"),
+    id: 3,
+    name: "Cáp chuyển từ Cổng USB sang PS2",
+    rating: 4,
+    price: 69900,
+    imgLink: require("../assets/dauchuyendoipsps21.png"),
   },
   {
-    name: "Hiểu lòng con trẻ",
-    producer: "Minh Long Book",
-    imgLink: require("../assets/hieu_long_con_tre.png"),
+    id: 2,
+    name: "Cáp chuyển từ Cổng USB sang PS2",
+    rating: 4,
+    price: 69900,
+    imgLink: require("../assets/daynguon1.png"),
   },
   {
-    name: "Donal Trump Thiên tài lãnh đạo",
-    producer: "Minh Long Book",
-    imgLink: require("../assets/trump_1.png"),
+    id: 1,
+    name: "Cáp chuyển từ Cổng USB sang PS2",
+    rating: 4,
+    price: 69900,
+    imgLink: require("../assets/giacchuyen1.png"),
   },
 ];
 export default function GridProduct() {
   return (
     <FlatList
-      className=" flex-1"
+      style ={{flex:1}}
       numColumns={2}
       data={data}
-      keyExtractor={(item) => item.name}
-      key={(item) => item.name}
+      keyExtractor={(item, i) => i}
       renderItem={({ item }) => (
         <View
           style={{
             display: "flex",
-            width: "100%",
-            padding: 1,
+            width: "50%",
+            padding: 2,
             paddingVertical: 3,
-            borderBottomWidth: 0.5,
-            borderColor: "gray",
-            borderStyle: "solid",
             justifyContent: "space-between",
           }}
         >
-          <Image source={item.imgLink} className={"w-40 h-20"} />
-          <View className="p-3 justify-evenly flex-1 self-start">
-            <Text className="text-black">{item.name}</Text>
-            <Text style={{ color: "gray" }}>
-              Shop
-              <Text className="text-black"> {item.producer}</Text>
-            </Text>
+          <Image source={item.imgLink} style ={{width:'100%', height:100}} />
+          <View className="p-3 justify-evenly flex-1 self-start px-5">
+            <Text className="text-black truncate w-[150px]">{item.name}</Text>
+            <View className="flex-row">
+              <AirbnbRating
+                showRating={false}
+                defaultRating={item.rating}
+                size={15}
+              />
+              <Text style={{ color: "gray" }}>(15)</Text>
+            </View>
+            <View className="flex-row">
+              <Text className="text-black font-bold"> {item.price} đ</Text>
+              <Text
+                style={{
+                  color: "gray",
+                  flex: 0.5,
+                  textAlign: "center",
+                }}
+              >
+                -39%
+              </Text>
+            </View>
           </View>
         </View>
       )}
--- components/ListProduct.js
+++ components/ListProduct.js
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, Image, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, FlatList, Image, TouchableOpacity } from "react-native";
 import React from "react";
 const data = [
   {
@@ -31,58 +31,49 @@ const data = [
     producer: "Minh Long Book",
     imgLink: require("../assets/hieu_long_con_tre.png"),
   },
-  
+  {
+    name: "Donal Trump Thiên tài lãnh đạo",
+    producer: "Minh Long Book",
+    imgLink: require("../assets/trump_1.png"),
+  },
+  {
+    name: "Ca nấu lẩu, nấu mì mi mini",
+    producer: "Devang",
+    imgLink: require("../assets/ca_nau_lau.png"),
+  },
 ];
-export default function ListProduct()  {
+export default function ListProduct() {
   return (
-    
     <FlatList
+      className="w-full flex-1"
       data={data}
       renderItem={({ item }) => (
-        <View style={styles.view}>
-          <Image style={styles.image} source={item.imgLink}/>
-          <View >
-            <Text style ={{}}>{item.name}</Text>
-            <Text style={{color:'gray'}}>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100%",
+            padding: 1,
+            paddingVertical: 3,
+            borderBottomWidth: 0.5,
+            borderColor: "gray",
+            borderStyle: "solid",
+            justifyContent: "space-between",
+          }}
+        >
+          <Image source={item.imgLink} className={"w-20 h-20"} />
+          <View className="p-3 justify-evenly flex-1 self-start">
+            <Text className="text-black">{item.name}</Text>
+            <Text style={{ color: "gray" }}>
               Shop
-              <Text> {item.producer}</Text>
+              <Text className="text-black"> {item.producer}</Text>
             </Text>
           </View>
-          <TouchableOpacity style={styles.button} >
-            <Text style ={{color:'white'}}>Chat</Text>
+          <TouchableOpacity className="self-center bg-red-600 px-2 py-1 h-10 w-28 justify-center items-center">
+            <Text className="text-white">Chat</Text>
           </TouchableOpacity>
         </View>
       )}
     />
   );
 }
-
-const styles = StyleSheet.create({
-    view:{
-        display: "flex",
-            flexDirection: "row",
-            width: "100%",
-            padding: 12,
-            paddingTop:5,
-            paddingVertical: 3,
-            borderBottomWidth: 1,
-            borderColor: "gray",
-            borderStyle: "solid",
-            justifyContent: "space-between",
-        
-    },
-    image:{
-        width:70,
-        height:70
-    },
-    button:{
-        backgroundColor:"red",
-        height:40,
-        width:70,
-        alignItems: "center",
-        alignSelf: 'center',
-        justifyContent:"center",
-        
-    },
-  });
-  
